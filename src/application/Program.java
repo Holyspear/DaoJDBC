680c635d907d37a782a43f9c4199fa7e923434ba
Holyspear <holyspear@gmail.com>
2022-07-03 17:00:34 -0300

    Criação das classes SellerDaoJDBC e DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,16 +2,23 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
-		Department departmentObj = new Department(1,"Books");
+
+		Department departmentObj = new Department(1, "Books");
+
 		System.out.println(departmentObj);
-		Seller sellerObj = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00,departmentObj);
+
+		Seller sellerObj = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.00, departmentObj);
+
+		SellerDao selleDao = DaoFactory.createSellerDao();
+
 		System.out.println(sellerObj);
 	}
 }
