499672075fde350e76cb6744ad68f337885ebd41
Holyspear <holyspear@gmail.com>
2022-07-03 19:25:48 -0300

    Implementação do método findByDepartment no SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,6 +16,13 @@ public class Program {
 		System.out.println("===== TEST 1: Seller FindById =====");		
 		Seller sellerFinded = sellerDao.findById(3);		
 		System.out.println(sellerFinded);
+		
+		System.out.println("\n===== TEST 2: Seller FindByDepartment =====");
+		Department department = new Department(2, null);
+		List<Seller> listSeller = sellerDao.findByDepartment(department);
+		for(Seller sellers : listSeller) {
+			System.out.println(sellers);
+		}
 
 	}
 }
