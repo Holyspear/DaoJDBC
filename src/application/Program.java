5adbbd132746d2b456db817efb423665ab7df269
Holyspear <holyspear@gmail.com>
2022-07-03 16:42:53 -0300

    Criação da classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department departmentObj = new Department(1,"Books");
-		System.out.println(departmentObj);		
+		System.out.println(departmentObj);
+		Seller sellerObj = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.00,departmentObj);
+		System.out.println(sellerObj);
 	}
 }
